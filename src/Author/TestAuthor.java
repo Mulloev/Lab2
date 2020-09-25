56c742e50b49f83fdf006dddfdad6417bd1fbe50
student <sashawas28@gmail.com>
2020-09-25 10:13:16 +0300

    Mulloev Aziz IVBO-10-19

---
+++ src/Author/TestAuthor.java
@@ -0,0 +1,14 @@
+package Lab2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author Andrey = new Author("Mulloev","alex@yandex.ru",'M');
+        Author unknown = new Author("unknown","123@goooglee.com");
+        Author  Joanne_Rowling =new Author("Joanne Rowling","jkrowling",'F');
+        Joanne_Rowling.setEmail("jkrowling@google.com");
+        System.out.println(Andrey.toString());
+        System.out.println(unknown.toString());
+        System.out.println(Joanne_Rowling.toString());
+
+    }
+}
---
+++ src/Ball/Test_Ball.java
@@ -0,0 +1,9 @@
+package Ball;
+public class Test_Ball {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(100, 100);
+        System.out.println(b1);
+        b1.move(40, 24);
+        System.out.println(b1);
+    }
+}
